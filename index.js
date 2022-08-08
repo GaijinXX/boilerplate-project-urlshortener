3e8092f43a4147383d5efbf03f6ff601821033fb
Akhundov Vahid <vaha.az@gmail.com>
2022-08-08 21:15:07 +0200

    URL Shortener

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
+const url = require('url');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.use(cors());
 
@@ -17,6 +21,25 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+let arr = [];
+
+app.post('/api/shorturl', function(req, res) {
+  let origurl = req.body.url;
+  dns.lookup(url.parse(origurl).host, (err, address, family) => {
+    if(err) {
+      res.json({ error: 'invalid url' });
+    }
+    else {
+      let shorturl = arr.push(origurl) - 1;
+      res.json({ original_url: origurl, short_url: shorturl });
+    }
+  });
+});
+
+app.get('/api/shorturl/:shorturl', function(req, res) {
+  res.redirect(arr[Number(req.params.shorturl)]);
 });
 
 app.listen(port, function() {
